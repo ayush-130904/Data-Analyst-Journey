54194e878c2f5e110e74f9d89737b5c44f46497f
Ayush Salunkhe <rsayush09@gmail.com>
2026-01-27 12:18:22 +0530

    Basic operations
    CREATE, show, etc

---
+++ SQL/lecture_1.sql
@@ -0,0 +1,14 @@
+show databases;
+CREATE DATABASE ayush;
+use ayush;
+Show tables;
+CREATE TABLE Students
+(
+Roll_no int,
+Name VARCHAR(30),
+Age int,
+Email VARCHAR(30),
+City VARCHAR(30)
+);
+desc Students;
+  
