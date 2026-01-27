d5eb8b045d0fc74210511886f6ed565129afd90f
Ayush Salunkhe <rsayush09@gmail.com>
2026-01-27 12:30:19 +0530

    SELECT, INSERT Queries

--- SQL/lecture_1.sql
+++ SQL/lecture_1.sql
@@ -10,5 +10,13 @@ Age int,
 Email VARCHAR(30),
 City VARCHAR(30)
 );
-desc Students;
+DESC Students;        #to show info for that table
+
+INSERT INTO Students
+(Roll_no, Name, Age, Email, City) VALUES
+(101, "Yash", 21, "yash123@gmail.com", "Kalyan"),
+(102, "Ashesh", 21, "ashesh123@gmail.com", "Kalyan"),
+(103, "Ritesh", 21, "ritesh123@gmail.com", "Kalyan");
+
+SELECT * FROM Students
   
