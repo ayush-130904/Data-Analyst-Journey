08749810c65f2be829cf26dad903b397877c8fe4
Ayush Salunkhe <rsayush09@gmail.com>
2026-01-29 11:59:04 +0530

    Filtering method

--- SQL/lecture_2.sql
+++ SQL/lecture_2.sql
@@ -1,5 +1,4 @@
 show databases;
-CREATE DATABASE ayush;
 use ayush;
 Show tables;
 CREATE TABLE Students
@@ -8,18 +7,19 @@ Roll_no int,
 Name VARCHAR(30),
 Age int,
 Marks int,
+Courses VARCHAR(30),
 Email VARCHAR(30),
 City VARCHAR(30)
 );
 DESC Students;        #to show info for that table
 
 INSERT INTO Students
-(Roll_no, Name, Age, Marks, Email, City) VALUES
-(101, "Yash", 21, 90, "yash123@gmail.com", "Kalyan"),
-(102, "Ashesh", 21, 75, "ashesh123@gmail.com", "Kalyan"),
-(103, "Ritesh", 21, 80, "ritesh123@gmail.com", "Kalyan"),
-(104, "Rohan", 21, 60, "rohan123@gmail.com", "Kalyan"),
-(105, "Tanmay", 21, 85, "rohan123@gmail.com", "Kalyan");
+(Roll_no, Name, Age, Marks, Courses, Email, City) VALUES
+(101, "Yash", 21, 90, "DS", "yash123@gmail.com", "Kalyan"),
+(102, "Ashesh", 21, 75, "DA", "ashesh123@gmail.com", "Kalyan"),
+(103, "Ritesh", 21, 80, "DS", "ritesh123@gmail.com", "Kalyan"),
+(104, "Rohan", 21, 60, "DA", "rohan123@gmail.com", "Kalyan"),
+(105, "Tanmay", 21, 85, "DS", "rohan123@gmail.com", "Kalyan");
 
 SELECT * FROM Students;
 
@@ -31,6 +31,7 @@ SELECT * FROM Students WHERE Marks <>80;     # not equal to - <>
 #Logical Operator
 SELECT * FROM Students WHERE Marks =60 OR Marks =80;      
 SELECT * FROM Students WHERE Marks BETWEEN 70 AND 80;
+SELECT * FROM Students WHERE NOT Marks =60;
 SELECT * FROM Students WHERE NOT Marks =60; 
 
  
@@ -38,3 +39,6 @@ SELECT * FROM Students WHERE NOT Marks =60;
 SELECT * FROM Students WHERE Name LIKE 'A%';      #Starting With 'A'
 SELECT * FROM Students WHERE Name LIKE '%sh';     #Ending with 'sh'
 SELECT * FROM Students WHERE Name LIKE '%a%';     #name containing 'a'
+
+SELECT * FROM Students WHERE Courses IN ("DS");   #Filtering DS students
+SELECT * FROM Students WHERE Courses NOT IN ("DS");    #Filtering DA students
